8ea47b1ff48fb1c25d7c368f174c8af3f836b5c1
ronny <ronnymwangi7@gmail.com>
2024-05-23 12:48:59 +0300

    done

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-import ipdb
+import ipdb           
 
 def plus_two(num):
-    num + 2
+    num = num + 2
+   
     return num
--- lib/ipdb_is_awesome.py
+++ lib/ipdb_is_awesome.py
@@ -11,4 +11,7 @@ def tracing_the_function():
         "The program froze before it could read me!"
     print(this_variable_hasnt_been_interpreted_yet)
 
+
+
+
 tracing_the_function()
